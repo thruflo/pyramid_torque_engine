8f0fcba5477b140ad9a2d5d80b559183498029ce
James Arthur <thruflo@gmail.com>
2015-06-16 17:15:06 +0100

    packaging: setup.py python egg boilerplate.

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pyramid_torque_engine',
+    version = '0.0.1',
+    description = 'Pyramid and nTorque based dual queue work engine system.',
+    author = 'James Arthur',
+    author_email = 'username: thruflo, domain: gmail.com',
+    url = 'http://github.com/thruflo/pyramid_torque_engine',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Pylons',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+    ],
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe = False,
+    install_requires=[
+        'fysom',
+        'pyramid_basemodel',
+        'pyramid_weblayer',
+        'transaction',
+        'zope.interface'
+    ],
+    tests_require = [
+        'coverage',
+        'nose',
+        'mock'
+    ]
+)
---
+++ src/pyramid_torque_engine/__init__.py
@@ -0,0 +1,1 @@
+# This `__init__.py` file makes this directory a Python package.
