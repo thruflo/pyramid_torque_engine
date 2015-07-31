3308feba993a9ec07084a097020d957f1a417e04
James Arthur <thruflo@gmail.com>
2015-08-01 00:03:59 +0100

    setup: bump version to debug pip issue.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pyramid_torque_engine',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Pyramid and nTorque based dual queue work engine system.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
