ba01f62010eab2a2d381fd6ea4d0c273e1659bc6
James Arthur <thruflo@gmail.com>
2015-08-03 18:53:12 +0100

    tests: transition tests.

--- src/pyramid_torque_engine/tests/ftests/test_events.py
+++ src/pyramid_torque_engine/tests/ftests/test_events.py
@@ -32,6 +32,16 @@ def get_handlers_for(dispatched, event, names_only=False):
         return names
     return handlers
 
+def triggered_events(result, event, names_only=False):
+    names = []
+    for item in result['response']['dispatched']:
+        data = json.loads(item['data'])
+        type_ = event.split(':')[0]
+        name = data.get(type_, None)
+        if name:
+            names.append(name)
+    return names
+
 class TestSubscriptions(boilerplate.AppTestCase):
     """Test ``on(context, events, operation, handler)`` rules."""
 
@@ -158,3 +168,108 @@ class TestSubscriptions(boilerplate.AppTestCase):
         handlers = get_handlers_for(dispatched, s.POKED, names_only=True)
         self.assertEqual(handlers[0], o.BEEP)
         self.assertEqual(len(handlers), 1)
+
+class TestTransitions(boilerplate.AppTestCase):
+    """Test ``after(context, operation, result, action)`` rules."""
+
+    @classmethod
+    def includeme(cls, config):
+        """Setup the test configuration."""
+
+        # Unpack.
+        allow, on, after = unpack.directives(config)
+
+        # Traversal.
+        config.add_engine_resource(model.Model, model.IContainer)
+        config.add_engine_resource(model.Foo, model.IFooContainer)
+
+        # Declare constants.
+        s.register(
+            'CREATED',
+            'STARTED',
+            'FINISHED',
+            'FAILED',
+            'DISPUTED',
+        )
+        a.register(
+            'START',
+            'FINISH',
+            'FAIL',
+            'DISPUTE',
+        )
+        o.register(
+            'DOIT',
+        )
+        r.register(
+            'SUCCESS',
+            'FAILURE',
+        )
+
+        # Get a handle on the model interface.
+        IModel = model.IModel
+        IFoo = model.IFoo
+
+        # Declare actions.
+        allow(IModel, a.START, (s.CREATED), s.STARTED)
+        allow(IModel, a.FINISH, (s.STARTED), s.FINISHED)
+        allow(IModel, a.FAIL, '*', s.FAILED)
+        allow(IModel, a.DISPUTE, '*', s.DISPUTED)
+
+        # When started, kick off some operation...
+        on(IModel, (s.STARTED), o.DOIT, ops.Dispatch())
+
+        # Bind the operation result to subsequent actions.
+        after(IModel, o.DOIT, r.SUCCESS, a.FINISH)
+        after(IModel, o.DOIT, r.FAILURE, a.FAIL)
+        after(IFoo, o.DOIT, r.FAILURE, a.DISPUTE)
+
+    def test_result(self):
+        """Test binding an operation result to a subsequent action."""
+
+        # Prepare.
+        app = self.factory()
+        request = self.getRequest(app)
+        context = model.factory(initial_state=s.STARTED)
+
+        # Reporting back success ...
+        notify = request.torque.engine
+        dispatch = notify.result(context, o.DOIT, r.SUCCESS)
+
+        # ... triggers a finish.
+        events = triggered_events(dispatch, 'action')
+        self.assertEqual(events[0], a.FINISH)
+        self.assertEqual(len(events), 1)
+
+    def test_other_result(self):
+        """Test binding an operation result to another action."""
+
+        # Prepare.
+        app = self.factory()
+        request = self.getRequest(app)
+        context = model.factory(initial_state=s.STARTED)
+
+        # Reporting back failure ...
+        notify = request.torque.engine
+        dispatch = notify.result(context, o.DOIT, r.FAILURE)
+
+        # ... triggers a fail.
+        events = triggered_events(dispatch, 'action')
+        self.assertEqual(events[0], a.FAIL)
+        self.assertEqual(len(events), 1)
+
+    def test_results_with_interface_inheritance(self):
+        """Test binding operation result with interface inheritance."""
+
+        # Prepare.
+        app = self.factory()
+        request = self.getRequest(app)
+        context = model.factory(cls=model.Foo, initial_state=s.STARTED)
+
+        # Reporting back failure ...
+        notify = request.torque.engine
+        dispatch = notify.result(context, o.DOIT, r.FAILURE)
+
+        # ... will result in a dispute.
+        events = triggered_events(dispatch, 'action')
+        self.assertEqual(events[0], a.DISPUTE)
+        self.assertEqual(len(events), 1)
